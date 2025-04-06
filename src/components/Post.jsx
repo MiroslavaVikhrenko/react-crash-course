6872c60298d4bbca59ebabe56c982dac9ca12115
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-05 18:59:58 -0600

    Use dynamic values in Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,6 +1,11 @@
+const names = ['Yumi', 'Kenji'];
+
 function Post() {
+    // Standard JS code to pick a random name for dynamic values 
+    const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+
     return <div>
-        <p>Mira</p>
+        <p>{chosenName}</p>
         <p>React.js is awesome!</p>
     </div>
 }
