1dfddf8cc7929545f1ea4f268b5401d14c876a7a
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-11 16:28:17 -0600

    Create PostsList.module.css custom styling for PostsList component and apply it

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,7 +1,8 @@
 import Post from './Post';
+import classes from './PostsList.module.css';
 
 function PostsList() {
-    return <ul>
+    return <ul className={classes.posts}>
         <Post author="Yumi" body="It's a great weather for running."/>
         <Post author="Kenji" body="Let's go for a long run!"/>
     </ul>
