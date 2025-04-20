85d328fb300f649a664e0164ce793d33bf42a6ac
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-20 15:14:48 -0600

    Create and use layout route

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client'
 import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import App from './App'
 import './index.css'
+import RootLayout from './routes/RootLayout';
 import NewPost from './components/NewPost';
 
 // Create a route configuration object for 'router' prop
 // createBrowserRouter() function takes an array which is a list of route definitions.
 // A route definition is an object
 const router = createBrowserRouter([
-  {path: '/', element: <App />}, // <our-domain>
-  {path: '/create-post', element: <NewPost />}, 
+  // layout route which wraps all routes no matter which path they have
+  {
+    path: '/', 
+    element: <RootLayout />, 
+    children:[ // nested routes:
+      {path: '/', element: <App />}, // <our-domain>
+      {path: '/create-post', element: <NewPost />}, // <our-domain>/create-post
+    ]
+  },  
 ]);
 
 // <RouterProvider /> enables Routing and tells React Router that it should watch the URL 
