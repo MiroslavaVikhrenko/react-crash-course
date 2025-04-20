bd5ef2910f67729b54a3a5409c2afc6510a14466
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-04-20 15:03:01 -0600

    Add routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 import App from './App'
 import './index.css'
+import NewPost from './components/NewPost';
 
+// Create a route configuration object for 'router' prop
+// createBrowserRouter() function takes an array which is a list of route definitions.
+// A route definition is an object
+const router = createBrowserRouter([
+  {path: '/', element: <App />}, // <our-domain>
+  {path: '/create-post', element: <NewPost />}, 
+]);
+
+// <RouterProvider /> enables Routing and tells React Router that it should watch the URL 
+// and start rendering different components for different paths.
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
